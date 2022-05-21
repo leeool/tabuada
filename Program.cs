b744fa8623b61d3ba03b22d033f58a53a51f1099
leeool <lgonsalez02@gmail.com>
2022-05-20 22:22:30 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int numero, contador = 1, resultado;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if  (numero <= 0){
+    Console.WriteLine("Digite um numero positivo ou diferente de 0");
+}
+
+else{
+    while (contador <= 10){
+        resultado = contador * numero;
+        Console.WriteLine($"{numero} x {contador++} = {resultado}");
+}
+
+}
